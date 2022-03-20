31771cadb67d10e881998b1689832ab1b0f6901a
Jiale <95jiale@gmail.com>
2022-03-20 21:48:13 +0800

    changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import logo from "./logo.svg";
 import "./App.css";
+import FormInput from "./components/FormInput";
+import React, { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [username, setUsername] = useState("");
+
+  console.log(username);
+  return (
+    <div className="App">
+      <form action="">
+        <FormInput placeholder="Username" setUsername={setUsername} />
+        <FormInput placeholder="Email" />
+        <FormInput placeholder="Full Name" />
+        <FormInput placeholder="Password" />
+      </form>
+    </div>
+  );
 }
 
 export default App;
